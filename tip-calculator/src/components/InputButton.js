97e708c6e2512c7783f89cac8f25e1da657e3445
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-03-02 20:35:33 -0800

    add advice from slack

--- tip-calculator/src/components/InputButton.js
+++ tip-calculator/src/components/InputButton.js
@@ -2,11 +2,45 @@ import React from 'react';
 
 
 const InputButton = ({tipPercent, setTipPercent, percentActive, setPercentActive, handlePercentChange, index}) => {
-
-  console.log("percent active: ", percentActive)
  
   return <button id={index} className={`inputBtn`} value={tipPercent} onClick={handlePercentChange}>{tipPercent}%</button>;
 };
 
 export default InputButton;
+
+/*
+const tips = [5, 10, 15, 25, 50];
+  const [selectedTip, setSelectedTip] = useState(15);
+
+  return (
+    <fieldset>
+      <legend>Choose Tip</legend>
+      {tips.map((tip) => (
+        <label key={tip} className={selectedTip === tip ? 'selected-tip' : ''}>
+          <input
+            checked={tip === selectedTip}
+            onChange={() => setSelectedTip(tip)}
+            type="radio"
+            name="tip-selector"
+            value={tip}
+          />
+          {tip}
+        </label>
+      ))}
+    </fieldset>
+  );
+
+input[type="radio"]:checked {
+  color: red;
+  ... whatever styles you want the checked one to have
+}
+
+// or on the class name
+.selected-tip {
+  some styles...
+}
+
+The key thing here is that the selectedTip state is not boolean, it's the actual tip id that you've selected. Then all of the rest of the logic is based on that
+
+*/
  
