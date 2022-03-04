b3ae4d4c6ed6f09ef3ee1f5502d637317ff247d1
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-03-03 18:58:32 -0800

    fix active button states

--- tip-calculator/src/components/InputButton.js
+++ tip-calculator/src/components/InputButton.js
@@ -1,46 +1,10 @@
 import React from 'react';
 
 
-const InputButton = ({tipPercent, setTipPercent, percentActive, setPercentActive, handlePercentChange, index}) => {
+const InputButton = ({tipPercent, handlePercentChange, selectedTip}) => {
  
-  return <button id={index} className={`inputBtn`} value={tipPercent} onClick={handlePercentChange}>{tipPercent}%</button>;
+  return <button className={selectedTip === tipPercent ? 'inputBtn-active' : 'inputBtn'} onClick={handlePercentChange} value={tipPercent}>{tipPercent}%</button>;
 };
 
+
 export default InputButton;
-
-/*
-const tips = [5, 10, 15, 25, 50];
-  const [selectedTip, setSelectedTip] = useState(15);
-
-  return (
-    <fieldset>
-      <legend>Choose Tip</legend>
-      {tips.map((tip) => (
-        <label key={tip} className={selectedTip === tip ? 'selected-tip' : ''}>
-          <input
-            checked={tip === selectedTip}
-            onChange={() => setSelectedTip(tip)}
-            type="radio"
-            name="tip-selector"
-            value={tip}
-          />
-          {tip}
-        </label>
-      ))}
-    </fieldset>
-  );
-
-input[type="radio"]:checked {
-  color: red;
-  ... whatever styles you want the checked one to have
-}
-
-// or on the class name
-.selected-tip {
-  some styles...
-}
-
-The key thing here is that the selectedTip state is not boolean, it's the actual tip id that you've selected. Then all of the rest of the logic is based on that
-
-*/
- 
--- tip-calculator/src/components/Inputs.js
+++ tip-calculator/src/components/Inputs.js
@@ -5,8 +5,7 @@ import InputButton from './InputButton'
 import InputCustomButton from './InputCustomButton'
 
 const Inputs = ({setTipPercent, setNumPeople, billTotal, setBillTotal, numPeople, setIsActive}) => {
-    const [percentActive, setPercentActive] = useState(undefined);
-    const [buttonState, setButtonState] = useState(true)
+    const [selectedTip, setSelectedTip] = useState(15);
     const [isZero, setIsZero] = useState(true);
 
     const handleBillInput = (e, numPeople) => {
@@ -40,39 +39,23 @@ const Inputs = ({setTipPercent, setNumPeople, billTotal, setBillTotal, numPeople
     }
 
     const handlePercentChange = (e) => {
-      console.log(e.target.value)
-      console.log(e.target)
-
-
-      let button_array = [...buttonGrid]
-      console.log(button_array)
-      console.log(button_array[0])
-      //let current_button={...button_array[]}
-      // for (let i = 0; i < button_array.length; i++) {
-      //   if (e.target.value === button_array[i].key) {
-      //     useState(button_array[i].key  )
-      //     console.log(`it's a match!`)
-      //   }
-      // }
-
-
+      setSelectedTip(e.target.value)         
       setTipPercent(e.target.value / 100)
     }
 
     //create input button grid
     const tips = [5, 10, 15, 25, 50];
 
-    const buttonGrid = tips.map((tip, index) => {
-      return <InputButton key={index} id={index} tipPercent={tip.toString()} setTipPercent={setTipPercent} percentActive={percentActive} setPercentActive={setPercentActive} handlePercentChange={handlePercentChange}/>
+    const buttonGrid = tips.map((tip) => {
+      return <InputButton 
+        key={tip} 
+        className={selectedTip === tip ? 'inputBtn-active' : 'inputBtn'}
+        tipPercent={tip.toString()}
+        selectedTip={selectedTip} 
+        setTipPercent={setTipPercent} 
+        handlePercentChange={handlePercentChange}/>
     });
-    
 
-/*
-buttonGrid is an array of objects
-
-splice(start, deleteCount, item) changes the contents 
-slice(start, end) non-inclusive of end, creates a copy
-*/
 
   return (
   <div className='inputs'>
